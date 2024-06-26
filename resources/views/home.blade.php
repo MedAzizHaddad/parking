d2da2aa17054b403431b604c0d1f36217cfb46d2
MedAzizHaddad <med.azizhaddad@gmail.com>
2024-06-26 23:17:34 +0100

    fix plain page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,48 +24,40 @@
                         <h3>Plain Page</h3>
                     </div>
 
-                    <div class="title_right">
-                        <div class="col-md-5 col-sm-5   form-group pull-right top_search">
-                            <div class="input-group">
-                                <input type="text" class="form-control" placeholder="Search for...">
-                                <span class="input-group-btn">
-                      <button class="btn btn-default" type="button">Go!</button>
-                    </span>
-                            </div>
-                        </div>
-                    </div>
+
                 </div>
+            </div>
 
-                <div class="clearfix"></div>
+            <div class="clearfix"></div>
 
-                <div class="row">
-                    <div class="col-md-12 col-sm-12  ">
-                        <div class="x_panel">
-                            <div class="x_title">
-                                <h2>Plain Page</h2>
-                                <ul class="nav navbar-right panel_toolbox">
-                                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                                    </li>
+            <div class="row">
+                <div class="col-md-12 col-sm-12  ">
+                    <div class="x_panel">
+                        <div class="x_title">
+                            <h2>Plain Page</h2>
+                            <ul class="nav navbar-right panel_toolbox">
+                                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                                </li>
 
-                                </ul>
-                                <div class="clearfix"></div>
-                            </div>
-                            <div class="x_content">
-                               HOME
-                            </div>
+                            </ul>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="x_content">
+                            HOME
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- /page content -->
-
-        <!-- footer content -->
-        <footer>
-            @include('layouts.Footer')
-        </footer>
-        <!-- /footer content -->
     </div>
+    <!-- /page content -->
+
+    <!-- footer content -->
+    <footer>
+        @include('layouts.Footer')
+    </footer>
+    <!-- /footer content -->
+</div>
 </div>
 
 <!-- jQuery -->
