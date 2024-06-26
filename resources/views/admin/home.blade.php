e14e8f3ff314457f714ed300e2877244909ba19f
MedAzizHaddad <med.azizhaddad@gmail.com>
2024-06-26 21:34:25 +0100

    extract layouts

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,1 +1,74 @@
-admin
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('layouts.Head')
+</head>
+
+<body class="nav-md">
+<div class="container body">
+    <div class="main_container">
+        <div class="col-md-3 left_col">
+            @include('layouts.Nav')
+        </div>
+
+        <!-- top navigation -->
+        @include('layouts.Header')
+        <!-- /top navigation -->
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+            <div class="">
+                <div class="page-title">
+                    <div class="title_left">
+                        <h3>Plain Page</h3>
+                    </div>
+
+                    <div class="title_right">
+                        <div class="col-md-5 col-sm-5   form-group pull-right top_search">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Search for...">
+                                <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">Go!</button>
+                    </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="clearfix"></div>
+
+                <div class="row">
+                    <div class="col-md-12 col-sm-12  ">
+                        <div class="x_panel">
+                            <div class="x_title">
+                                <h2>Plain Page</h2>
+                                <ul class="nav navbar-right panel_toolbox">
+                                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                                    </li>
+
+                                </ul>
+                                <div class="clearfix"></div>
+                            </div>
+                            <div class="x_content">
+                                ADMIN
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /page content -->
+
+        <!-- footer content -->
+        <footer>
+            @include('layouts.Footer')
+        </footer>
+        <!-- /footer content -->
+    </div>
+</div>
+
+<!-- jQuery -->
+@include('layouts.Jquery')
+</body>
+</html>
---
+++ resources/views/layouts/Jquery.blade.php
@@ -0,0 +1,10 @@
+<script src="{{ asset('import/jquery/dist/jquery.min.js') }}"></script>
+<!-- Bootstrap -->
+<script src="{{ asset('import/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+<!-- FastClick -->
+<script src="{{ asset('import/fastclick/lib/fastclick.js') }}"></script>
+<!-- NProgress -->
+<script src="{{ asset('import/nprogress/nprogress.js') }}"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="{{ asset('build/js/custom.min.js') }}"></script>
